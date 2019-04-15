e0afac00fe06fc4be51cc162662c4325ef0ee132
megan0302 <ching90302.jp@gmail.com>
2019-04-15 22:53:59 +0800

    [Modify]

--- src/App.js
+++ src/App.js
@@ -1,42 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
-import TodoList from './TodoList';
+import AddTodoList from './AddTodoList';
+import Todo from './Todo';
 
 class App extends Component {
-  
+
   constructor() {
     super();
     this.state = {
-      itemAdded:'',
-      itemList:['Go Shopping', 'Eat Dinner', 'Say Hello'],
+      itemList:[
+        { 
+          id: 1,
+          itemName: 'Go Shopping',
+          isCompleted: true
+        },
+        {
+          id: 2,
+          itemName: 'Eat Dinner',
+          isCompleted: false
+        },
+        {
+          id: 3,
+          itemName: 'Say Hello',
+          isCompleted: false
+        }],
+        incre_id: 3,
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleComplete = this.handleComplete.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({itemAdded: event.target.value});
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
+  handleAdd(add_item)
+  {
+    const new_id = this.state.incre_id + 1;
     this.setState({
-      itemAdded: '',
-      itemList: [...this.state.itemList, this.state.itemAdded]
+      itemList: [...this.state.itemList, {id:new_id, itemName:add_item, isCompleted:false}],
+      incre_id: new_id
+    });
+  }
+
+  handleDelete(item_id){
+    this.setState({
+      itemList:this.state.itemList.filter((a) => (a.id)!== item_id)
+    });
+  }
+
+  handleComplete(item_id){
+    this.setState({
+      todos: this.state.itemList.map(todo => {
+        if(todo.id === item_id)
+          todo.isCompleted = !todo.isCompleted;
+        return todo;
+      })
     });
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Things To Do:<br/>
-            <input type="text" value={this.state.itemAdded} onChange={this.handleChange}/>
-            </label>
-            <input type="submit" value="Add" />
-          </form>
-          <TodoList itemList={this.state.itemList} />
+        <AddTodoList handleAdd={this.handleAdd}/>
+        <ul>
+          <Todo itemList={this.state.itemList} handleDelete={this.handleDelete} handleComplete={this.handleComplete} />
+        </ul>
       </div>
     );
   }
